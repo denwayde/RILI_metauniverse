622de4c3a7f19367de070f0986d2a31d86b8e68c
den wayde <den.wayde@yandex.ru>
2024-03-16 16:06:23 +0500

    kapec kakoi to

--- front_end/src/pages/admins_components/Edit.jsx
+++ front_end/src/pages/admins_components/Edit.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Edit_input(props) {
+    let [newValue, setNewValue] = useState(props.name)
+    function handleInput(e){
+        setNewValue(e.target.value)
+    }
     return (
         <div class="input-group flex-nowrap">
-            {/* <span class="input-group-text" id="addon-wrapping">@</span> */}
-            <input type="text" class="form-control"  aria-label="Username" aria-describedby="addon-wrapping" value={props.name} />
+            <input type="text" class="form-control" onChange={handleInput} aria-label="Username" aria-describedby="addon-wrapping" value={newValue} />
         </div>
     )
 }
